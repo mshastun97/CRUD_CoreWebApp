4280793cc93ece8c2b0c613bd6d959b2b7f1a278
Mykhailo Shastun <mshastun@oncologyanalytics.com>
2019-07-11 15:44:41 +0300

    Added Home page(only markup)

---
+++ CRUD_CoreWebApp/MyAngularApp/src/app/Home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home-root',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+    title: 'My personal blog';
+}
